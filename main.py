1f9a9194097ef98669dec74abeaa415659cf4ca7
krishjindal1 <official827227@gmail.com>
2026-06-08 14:07:19 +0000

    added upload document tab with button

--- main.py
+++ main.py
@@ -112,126 +112,160 @@ with st.sidebar:
     
 
 
+
+
 # Main app
 st.title("📄 AI Document Analyzer")
+
+tab1, tab2 = st.tabs(
+    ["📝 Text Document", "📂 Upload Document"]
+)
+
+with tab1:
 #Text Box to paste the document to be analyzed
-document_text = st.text_area(
+    document_text = st.text_area(
     "Paste your text here:",
     height=300,
     placeholder="Enter the text you want to summarize or rewrite."
 )
 
 
-# Button to trigger the analysis
-if st.button("Analyze Document"):
-    
-    if not document_text.strip():
-        st.warning("Please enter a document.")
-    
-    else:
-        with st.status("📄 Processing Document...", expanded=True) as status:
+    # Button to trigger the analysis
+    analyze_button = st.button("Analyze Document")
+    #analyze_button Logic
+    if analyze_button:
+        if not document_text.strip():
+            st.warning("Please enter a document.")
+        
+        else:
+            with st.status("📄 Processing Document...", expanded=True) as status:
 
-            st.write("Generating summary...")
-    
+                st.write("Generating summary...")
+        
 
-    # Call the Ollama API to get the summary or rewrite based on the user's choice
-            summary_response = ollama.chat(
-                    model=selected_model,
-                
-                messages=[
-            {
-                "role": "system",
-                "content": "You are an expert document analyst."
-            },
-            {
-                "role": "user",
-                #prompt for better summarized content.
-                "content": f"""
-                    Generate a {summary_size} summary. 
-
-                        Requirements:
-                        - Capture all key points.
-                        - Preserve important facts.
-                        - Do not omit critical information.
-                        - Use clear headings where appropriate.
-                        - Do not leave the summary unfinished.
-                        
-                        Document:
-                        {document_text}
-                        """
-                            }
-        ],
-                options={
-                    "temperature": 0.2,
-                    "num_predict": 2000 # set a high token limit to avoid the summary being cut off due to token limits.
-                }
-        )
-
-            summary = summary_response["message"]["content"]
-            st.write("Summary generated ✅")
-
-            
-            
-            st.write("Generating rewritten content...")
-            config = tone_config[tone]
-            instruction = config["instruction"]
-
-
-            #Api call to rewrite the content based on the selected tone and rewrite target 
-            rewrite_response = ollama.chat(
-                model=selected_model,
-                messages=[
+        # Call the Ollama API to get the summary or rewrite based on the user's choice
+                summary_response = ollama.chat(
+                        model=selected_model,
+                    
+                    messages=[
                 {
                     "role": "system",
-                    "content": "You are an expert content rewriter."
+                    "content": "You are an expert document analyst."
                 },
-
                 {
                     "role": "user",
+                    #prompt for better summarized content.
                     "content": f"""
-                        You are an expert editor.
+                        Generate a summary.
 
-                        Task:
-                            Rewrite the content using a {tone} tone.
-
-                        Tone Guidelines:
-                            {instruction}
+                            Target Length:
+                                {target_length} 
 
                             Requirements:
-                            - Preserve all important information.
-                            - Do not change factual content.
-                            - Improve readability and organization.
-                            - Keep the rewritten version approximately the same length.
-                            - Use headings or bullet points when appropriate.
-                            - Do not leave the rewritten version unfinished.
+                            - Capture all key points.
+                            - Preserve important facts.
+                            - Do not omit critical information.
+                            - Use clear headings where appropriate.
+                            - Do not leave the summary unfinished.
                             
-
-                        Content:
-                        {summary if rewrite_target == "Summary" else document_text}
-                        """
-                }
+                            Document:
+                            {document_text}
+                            """
+                                }
             ],
-                options={
-                    "temperature": temperature,
-                }
-        )
+                    options={
+                        "temperature": 0.2,
+                        "num_predict": 2000 # set a high token limit to avoid the summary being cut off due to token limits.
+                    }
+            )
 
-            rewritten_text = rewrite_response["message"]["content"]
-            st.write("Rewrite completed ✅")
+                summary = summary_response["message"]["content"]
+                st.success("Summary generated")
 
-            status.update(
-                        label="✅ Analysis Complete",
-                        state="complete",
-                        
-                    )
+                
+                
+                st.write("Generating rewritten content...")
+                config = tone_config[tone]
+                instruction = config["instruction"]
+
+
+                #Api call to rewrite the content based on the selected tone and rewrite target 
+                rewrite_response = ollama.chat(
+                    model=selected_model,
+                    messages=[
+                    {
+                        "role": "system",
+                        "content": "You are an expert content rewriter."
+                    },
+
+                    {
+                        "role": "user",
+                        "content": f"""
+                            You are an expert editor.
+
+                            Task:
+                                Rewrite the content using a {tone} tone.
+
+                            Tone Guidelines:
+                                {instruction}
+
+                                Requirements:
+                                - Preserve all important information.
+                                - Do not change factual content.
+                                - Improve readability and organization.
+                                - Keep the rewritten version approximately the same length.
+                                - Use headings or bullet points when appropriate.
+                                - Do not leave the rewritten version unfinished.
+                                
+
+                            Content:
+                            {summary if rewrite_target == "Summary" else document_text}
+                            """
+                    }
+                ],
+                    options={
+                        "temperature": temperature,
+                    }
+            )
+
+                rewritten_text = rewrite_response["message"]["content"]
+                st.success("Rewritten text generated")
+
+                status.update(
+                            label="✅ Analysis Complete",
+                            state="complete",
+                            
+                        )
 
 
 
-         # Display the summary and rewritten text in separate tabs
-        tab1, tab2 = st.tabs(["📄 Summary", "✍️ Rewritten"])
-        with tab1:
-                st.subheader("📄 Summary")
-                st.write(summary)
-        with tab2:
-                st.subheader("✍️ Rewritten Text")
-                st.write(rewritten_text)
+            # Display the summary and rewritten text in separate tabs
+            sum_tab1, rewrite_tab2 = st.tabs(["📄 Summary", "✍️ Rewritten"])
+            with sum_tab1:
+                    st.subheader("📄 Summary")
+                    st.write(summary)
+            with rewrite_tab2:
+                    st.subheader("✍️ Rewritten Text")
+                    st.write(rewritten_text)
+
+
+
+with tab2:
+     uploaded_file = st.file_uploader(
+        "Upload a document",
+        type=["pdf", "docx", "pptx", "txt"]
+    )
+    # Button to trigger the analysis of the uploaded document
+     analyze_upload = st.button(
+        "Analyze Uploaded Document"
+    )
+     #Analyze_Upload Button Logic
+     if analyze_upload:
+        if not uploaded_file:
+            st.warning("Please upload a document.")
+        
+        else:
+            st.write("Document uploaded successfully ✅")
+            st.write(f"File Name: {uploaded_file.name}")
+            st.write(f"File Type: {uploaded_file.type}")
+            st.write(f"Size: {uploaded_file.size / 1024:.2f} KB")
